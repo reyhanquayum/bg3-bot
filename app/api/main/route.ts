5459dfda5408a0577c90e31a5ca01f0cb4705962
reyhanquayum <reyhan.quayum@gmail.com>
2024-12-23 14:41:42 -0500

    api setup

---
+++ app/api/main/route.ts
@@ -0,0 +1,3 @@
+/*
+retrieving documents from vector store, split text into chunks and return as json
+*/
---
+++ app/api/retrieval/route.ts
@@ -0,0 +1,4 @@
+/*
+Code for chat prompts, retrieval, and other user interactions here
+Langchain
+*/
